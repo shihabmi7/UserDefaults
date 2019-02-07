0cd34e8ab2e3c06364830ab8f55c1b20c7889453
Shihab Uddin <shihab.apps.developer@gmail.com>
2019-02-07 12:57:10 +0600

    Screen Shot Added

--- UserDefaults/AppDelegate.swift
+++ UserDefaults/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  UserDefaults
 //
-//  Created by Raihan on 6/2/19.
+//  Created by Shihab on 6/2/19.
 //  Copyright © 2019 shihab. All rights reserved.
 //
 
@@ -22,23 +22,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+         print("applicationWillResignActive")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    print("applicationWillEnterForeground")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+         print("applicationWillEnterForeground")
+        
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        print("applicationDidBecomeActive")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        print("applicationWillTerminate")
     }
 
 
--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -2,14 +2,31 @@
 //  ViewController.swift
 //  UserDefaults
 //
-//  Created by Raihan on 6/2/19.
+//  Created by Shihab on 6/2/19.
 //  Copyright © 2019 shihab. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var lblShowName: UILabel!
+    @IBOutlet weak var tfName: UITextField!
+    
+    @IBAction func saveName(_ sender: Any) {
+        
+       saveUserName(self.tfName.text!)
+    }
+    
+    
+    @IBAction func showName(_ sender: Any) {
+        
+        let preference = UserDefaults.standard
+       
+        let value = preference.string(forKey: "name") ??  ""
+        lblShowName.text = "Your Name is : "+value
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +37,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func saveUserName(_ name: String){
+        
+        let appDefualt = UserDefaults.standard
+        
+        appDefualt.set(name, forKey: "name")
+        
+        //appDefualt.synchronize()
+
+    }
+    
 
 }
 
